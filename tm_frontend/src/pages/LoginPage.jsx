d14a8e6d5f6cce96f1b2b78ddc3dcf899bf46da3
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-06-06 10:10:55 +0530

    created login page

--- tm_frontend/src/pages/LoginPage.jsx
+++ tm_frontend/src/pages/LoginPage.jsx
@@ -1,3 +1,6 @@
+import { FcGoogle } from "react-icons/fc";
+import {Link} from "react-router-dom";
+
 export default function LoginPage() {
     return(
         <div className = "w-full h-screen flex">
@@ -22,10 +25,27 @@ export default function LoginPage() {
                     <label className = "block text-gray-700 font-bold mb-2 text-xl">Password</label>
                     <input className = "w-[700px] h-[50px] shadow rounded py-2 px-3 bg-[#F5EDD6] text-gray-700" id="password" type="password" placeholder="Enter your password" />
                 </div>
+                
+                <div>
+                    <button className = "w-[700px] h-[50px] bg-[#1A7A6E] hover:bg-[#0D4F46] text-white text-xl font-bold rounded mt-10 ml-20">Sign In</button>
+                </div>
+
+                <div className = "flex items-center justify-center pt-10">
+                    <hr className = "w-[150px] border-black" />
+                    <p className = "px-4 text-black">or continue with</p>
+                    <hr className = "w-[150px] border-black" />
+                </div>
+
+                <div className="flex pt-10">
+                    <button className="w-[700px] h-[50px] bg-[#1A7A6E] hover:bg-[#0D4F46] text-xl text-white font-bold rounded ml-20 flex items-center justify-center gap-2">
+                        <FcGoogle size={24} />Google</button>
+                </div>
+
+                <div className = "flex relative pt-10 pl-20">
+                    <p className = "text-black text-xl">Don't have an account? <Link to="/signup" className="text-[#1A7A6E] hover:text-[#0D4F46]">Sign up</Link></p>
+                </div>
 
             </div>
-
-            
         </div>
     )
 }
