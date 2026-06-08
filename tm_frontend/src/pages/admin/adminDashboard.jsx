1f7626ad524c4219ae30b1edfea9593eae5b96fb
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-06-09 02:03:05 +0530

    added icons

--- tm_frontend/src/pages/admin/adminDashboard.jsx
+++ tm_frontend/src/pages/admin/adminDashboard.jsx
@@ -1,11 +1,21 @@
 import {Link} from 'react-router-dom'
+import { MdSpaceDashboard } from "react-icons/md";
+import { RiContactsBook3Fill } from "react-icons/ri";
+import { LiaHotelSolid } from "react-icons/lia";
+import { FaCar } from "react-icons/fa";
+import { IoBook } from "react-icons/io5";
+import { LuActivity } from "react-icons/lu";
+import { FaUsers } from "react-icons/fa";
+import { MdReviews } from "react-icons/md";
+import { TbReportSearch } from "react-icons/tb";
+import { IoSettings } from "react-icons/io5";
 
 export default function AdminDashboard(){
     return(
         <div className = "flex">
             {/* left side */}
             <div className = "w-[300px] h-screen bg-black">
-                <div className = "flex justify-center items-center mt-5 gap-5">
+                <div className = "flex justify-center items-center  mt-5 gap-5">
                     <img src = "logo.png" className = "w-[80px] h-[80px] ml-10" />
                     <span className = "text-[#C9A84C] text-4xl font-bold">Go Lanka</span>
                 </div>
@@ -16,18 +26,18 @@ export default function AdminDashboard(){
                 {/*main buttons */}
                 <div className = "flex flex-col">
                     <span className = "text-white ml-10 font-bold text-2xl">MAIN</span>
-                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl">Dashboard</Link>
-                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl">Bookings</Link>
-                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl">Hotels</Link>
-                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl">Vehicles</Link>
-                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl">Tour Packages</Link>
-                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl">Activities</Link>
+                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl flex items-center gap-5"><MdSpaceDashboard />Dashboard</Link>
+                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl flex items-center gap-5"><RiContactsBook3Fill />Bookings</Link>
+                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl flex items-center gap-5"><LiaHotelSolid />Hotels</Link>
+                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl flex items-center gap-5"><FaCar />Vehicles</Link>
+                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl flex items-center gap-5"><IoBook />Tour Packages</Link>
+                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl flex items-center gap-5"><LuActivity />Activities</Link>
 
                     <span className = "text-white ml-10 mt-3 corsor-pointer font-bold text-2xl">MANAGEMENT</span>
-                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl">Users</Link>
-                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl">Reviews</Link>
-                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl">Reports</Link>
-                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl">Settings</Link>
+                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl flex items-center gap-5"><FaUsers />Users</Link>
+                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl flex items-center gap-5"><MdReviews />Reviews</Link>
+                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl flex items-center gap-5"><TbReportSearch />Reports</Link>
+                    <Link to = "/" className = "text-[#C9A84C] ml-10 mt-3 corsor-pointer text-xl flex items-center gap-5"><IoSettings />Settings</Link>
                 </div>
 
                 <hr className = "border-amber-50 m-5"  />
