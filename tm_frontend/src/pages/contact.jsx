0211e15b303f9fb45b0ce0ba3e7c57cf48d95ee2
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-07-05 21:46:56 +0530

    added nav bar in contact page

--- tm_frontend/src/pages/contact.jsx
+++ tm_frontend/src/pages/contact.jsx
@@ -1,9 +1,28 @@
 import {FaPhoneAlt, FaEnvelope, FaMapMarkerAlt, FaClock, FaPaperPlane, FaHeadset, FaGlobeAsia, FaComments} from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Contact() {
   return (
     <div className="bg-[#F5EDD6] from-[#f7faf9] via-white to-[#eef6f4] min-h-screen">
       {/* HERO SECTION */}
+      <div className="w-full h-[100px] bg-black flex items-center pl-20 gap-5">
+          <img src="logo.png" alt="Logo" className="w-[80px] h-[80px]" />
+          <h1 className="text-5xl text-[#C9A84C] font-bold"> GoLanka </h1>
+
+          <div className="flex gap-10 ml-[400px]">
+              <Link to="/*" className="text-white text-xl hover:text-[#C9A84C] cursor-pointer"> Home </Link>
+              <Link to="/hotels" className="text-white text-xl hover:text-[#C9A84C] cursor-pointer"> Hotels </Link>
+              <Link to="/vehicles" className="text-white text-xl hover:text-[#C9A84C] cursor-pointer"> Vehicles </Link>
+              <Link to="/tours" className="text-white text-xl hover:text-[#C9A84C] cursor-pointer"> Tours </Link>
+              <Link to="/guides" className="text-white text-xl hover:text-[#C9A84C] cursor-pointer"> Guides </Link>
+              <Link to="/contact" className="text-white text-xl hover:text-[#C9A84C] cursor-pointer"> Contact </Link>
+          </div>
+
+          <div className="flex gap-5 mr-20">
+            <button className="w-[150px] h-[40px] bg-[#C9A84C] hover:bg-[#F5EDD6] text-black text-lg font-bold rounded-3xl ml-20 cursor-pointer"><Link to="/">My Bookings</Link></button>
+          </div>
+      </div>
+
       <div className="relative bg-gradient-to-r from-[#0D4F46] via-[#17675B] to-[#0D4F46] text-white overflow-hidden">
         <div className="absolute w-96 h-96 bg-[#C9A84C]/20 rounded-full -top-20 -left-20 blur-3xl"></div>
         <div className="absolute w-80 h-80 bg-white/10 rounded-full bottom-0 right-0 blur-3xl"></div>
