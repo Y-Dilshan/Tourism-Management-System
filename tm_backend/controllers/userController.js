2c00cd47e393e362fc3ef1c26a0126458c30fd72
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-07-12 01:49:00 +0530

    user create function

--- tm_backend/controllers/userController.js
+++ tm_backend/controllers/userController.js
@@ -1,3 +1,13 @@
 import User from "../models/User.js";
 import express from "express";
 
+export const createUser = async (req, res) =>{
+    try{
+        const { name, email, password } = req.body;
+        const newUser = new User({ name, email, password });
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
