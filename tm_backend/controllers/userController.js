7b559ea572a64d3c23a9851b048431428584d0be
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-07-13 20:35:39 +0530

    created deleteusers function

--- tm_backend/controllers/userController.js
+++ tm_backend/controllers/userController.js
@@ -53,3 +53,13 @@ export const deleteUser = async (req, res) => {
         res.status(400).json({ message: error.message });
     }
 }
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
