7ea03887a08cadcf6a2ad29719e3fff5c6f7c997
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-07-14 00:55:24 +0530

    created createGuide function

--- tm_backend/controllers/giudeController.js
+++ tm_backend/controllers/giudeController.js
@@ -8,3 +8,14 @@ export const getAllGuides = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+export const createGuide = async (req, res) => {
+  const { name, email, password } = req.body;
+  const newGuide = new guide({ name, email, password });
+  try {
+    await newGuide.save();
+    res.status(201).json(newGuide);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
