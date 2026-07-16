b5d14c48253efc53cd89ac585e238d9503326109
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-07-16 20:43:05 +0530

    created createHotel function

--- tm_backend/controllers/hotelController.js
+++ tm_backend/controllers/hotelController.js
@@ -8,3 +8,14 @@ export const getAllHotels = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const createHotel = async (req, res) => {
+  const { name, location, price } = req.body;
+  const newHotel = new hotel({ name, location, price });
+    try {
+    await newHotel.save();
+    res.status(201).json(newHotel);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  } 
+};
