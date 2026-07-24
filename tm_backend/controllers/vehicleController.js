076f2e36eeeb5f82f570a0ed9113537aba460ee2
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-07-24 22:28:52 +0530

    created getVehicles function

--- tm_backend/controllers/vehicleController.js
+++ tm_backend/controllers/vehicleController.js
@@ -10,3 +10,12 @@ export const createVehicle = async (req, res) => {
         res.status(400).json({ message: error.message });
     }   
 };
+
+export const getVehicles = async (req, res) => {
+    try {
+        const vehicles = await Vehicle.find();
+        res.status(200).json(vehicles);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
