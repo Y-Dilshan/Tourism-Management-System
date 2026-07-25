396b216c3f13011e03e04cad0eb1cd629ec1aa9f
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-07-25 22:41:05 +0530

    created vehicleRouter file

--- tm_backend/Routes/vehicleRouter.js
+++ tm_backend/Routes/vehicleRouter.js
@@ -1,2 +1,10 @@
-import vehicleController from '../controllers/vehicleController.js';
+import { createVehicle, updateVehicle, getVehicleById, getVehicles, deleteVehicle } from '../controllers/vehicleController.js';
 import express from 'express';
+
+const router = express.Router();
+
+router.post('/', createVehicle);
+router.get('/:id', getVehicleById);
+router.put('/:id', updateVehicle);
+router.delete('/:id', deleteVehicle);
+router.get('/', getVehicles);
