87da9442d456ab2d5961bba7eaf97d4affa465aa
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-07-26 23:08:15 +0530

    created create function in tourController.js

--- tm_backend/controllers/tourController.js
+++ tm_backend/controllers/tourController.js
@@ -1,2 +1,18 @@
 import Tour from '../models/tour.js';
 
+export const createTour = async (req, res) => {
+    try {
+        const newTour = await Tour.create(req.body);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        });
+    }
+};
