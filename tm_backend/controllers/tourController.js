a5dc2911762a6421dd9d75b624befd792d7bc5ac
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-07-28 22:26:04 +0530

    created getAllTours function

--- tm_backend/controllers/tourController.js
+++ tm_backend/controllers/tourController.js
@@ -16,3 +16,21 @@ export const createTour = async (req, res) => {
         });
     }
 };
+
+export const getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find();
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        });
+    }
+};
