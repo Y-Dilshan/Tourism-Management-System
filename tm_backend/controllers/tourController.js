fac1becf8a40723c8cd1140f70c377627d429ba3
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-07-28 22:27:01 +0530

    created getTour function

--- tm_backend/controllers/tourController.js
+++ tm_backend/controllers/tourController.js
@@ -34,3 +34,26 @@ export const getAllTours = async (req, res) => {
         });
     }
 };
+
+export const getTour = async (req, res) => {
+    try {
+        const tour = await Tour.findById(req.params.id);
+        if (!tour) {
+            return res.status(404).json({
+                status: 'fail',
+                message: 'Tour not found'
+            });
+        }
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        });
+    }
+};
